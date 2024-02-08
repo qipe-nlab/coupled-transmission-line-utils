01bc74bed91f7b21cc57b00ca03370c2c598284a
Peter <peter.spring@riken.jp>
2024-02-08 17:04:23 +0900

    small changes

--- RIKEN_low_freq_double_resonator_pattern_user_params.py
+++ RIKEN_low_freq_double_resonator_pattern_user_params.py
@@ -10,7 +10,6 @@ COMSOL_readout_filter_shared_params = \
         'meander_spacing': 45,
         'curvatur_radius': 60,
         'center_line_width': 20,
-        'meander_curves': 5,
         'readout_via_pad_deformation': 37.5 
     }
 
@@ -34,58 +33,66 @@ COMSOL_filter_shared_params = \
 
 COMSOL_readout_user_params_A = \
     {   'end_sec_len': 100,
-        'fine_tuning_end': 195, 
-        'Q_diam': 260,
-        'inner_pad_deformation': 42.5,
+        'fine_tuning_end': 178, 
+        'Q_diam': 270,
+        'inner_pad_deformation': 35,
         'double_resonator_spacing': 5.5,
         'start_curve_radius': 110,
+        'meander_curves': 5,
     }
 
 COMSOL_filter_user_params_A = \
     {   'wiggle_n': 0,
         'wiggle_length': 90,
+        'meander_curves': 5,
     }
 
 COMSOL_readout_user_params_B = \
-    {   'end_sec_len': 255,
-        'fine_tuning_end': 222, 
-        'Q_diam': 126.5,
-        'inner_pad_deformation': 25,
+    {   'end_sec_len': 400,
+        'fine_tuning_end': 150, 
+        'Q_diam': 240,
+        'inner_pad_deformation': 17.5,
         'double_resonator_spacing': 3.8, #3.8
         'start_curve_radius': 60,
+        'meander_curves': 4,
     }
 
 COMSOL_filter_user_params_B = \
     {   'wiggle_n': 3,
         'wiggle_length': 82.5,
+        'meander_curves': 4,
     }
 
 COMSOL_readout_user_params_C = \
-    {   'end_sec_len': 100,
-        'fine_tuning_end': 175, 
-        'Q_diam': 130,
-        'inner_pad_deformation': 25,
+    {   'end_sec_len': 450,
+        'fine_tuning_end': 150, 
+        'Q_diam': 250,
+        'inner_pad_deformation': 17.5,
         'double_resonator_spacing': 4.2, # 4.2
         'start_curve_radius': 60,
+        'meander_curves': 4,
     }
 
 COMSOL_filter_user_params_C = \
     {   'wiggle_n': 3,
         'wiggle_length': 85,
+        'meander_curves': 4,
     }
 
 COMSOL_readout_user_params_D = \
-    {   'end_sec_len': 333,
-        'fine_tuning_end': 238, 
-        'Q_diam': 150.5,
-        'inner_pad_deformation': 42.5,
+    {   'end_sec_len': 100,
+        'fine_tuning_end': 220, 
+        'Q_diam': 280,
+        'inner_pad_deformation': 35,
         'double_resonator_spacing': 5.5,
         'start_curve_radius': 130,
+        'meander_curves': 5,
     }
 
 COMSOL_filter_user_params_D = \
     {   'wiggle_n': 0,
         'wiggle_length': 90,
+        'meander_curves': 5,
     }
 
 def generate_readout_param_dict(unit):
